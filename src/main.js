a86fa8a6a9b67c76c3ce1f419db6da11bd9a20f5
kawsurilu <kawsurilu@yahoo.com>
2020-10-03 11:02:43 +0600

    -static build for cPanel

--- src/main.js
+++ src/main.js
@@ -16,76 +16,94 @@ Vue.use(VueRouter);
 Vue.use(VueSidebarMenu);
 
 Vue.directive("click-outside", {
-  bind: function(el, binding, vnode) {
-    el.clickOutsideEvent = function(event) {
-      // here I check that click was outside the el and his childrens
-      if (!(el == event.target || el.contains(event.target))) {
-        // and if it did, call method provided in attribute value
-        vnode.context[binding.expression](event);
-      }
-    };
-    document.body.addEventListener("click", el.clickOutsideEvent);
-  },
-  unbind: function(el) {
-    document.body.removeEventListener("click", el.clickOutsideEvent);
-  },
+    bind: function (el, binding, vnode) {
+        el.clickOutsideEvent = function (event) {
+            // here I check that click was outside the el and his childrens
+            if (!(el == event.target || el.contains(event.target))) {
+                // and if it did, call method provided in attribute value
+                vnode.context[binding.expression](event);
+            }
+        };
+        document.body.addEventListener("click", el.clickOutsideEvent);
+    },
+    unbind: function (el) {
+        document.body.removeEventListener("click", el.clickOutsideEvent);
+    },
 });
 
 const routes = [
-  {
-    path: "/",
-    component: HomePage,
-    meta: {
-      title: "MainstreamSource | Home",
-      metaTags: [
-        {
-          name: "description",
-          content: "MainstreamSource Home page",
+    {
+        path: "/",
+        component: HomePage,
+        meta: {
+            title: "MainstreamSource | Home",
+            metaTags: [
+                {
+                    name: "description",
+                    content: "MainstreamSource Home page",
+                },
+                {
+                    property: "og:description",
+                    content: "MainstreamSource Home page",
+                },
+            ],
         },
-        {
-          property: "og:description",
-          content: "MainstreamSource Home page",
-        },
-      ],
     },
-  },
-  {
-    path: "/genres/:id/tracks",
-    component: TrackList,
-    meta: {
-      title: "MainstreamSource | Genre {Name} |Track List",
-      metaTags: [
-        {
-          name: "description",
-          content: "MainstreamSource Genra Track List page",
+    {
+        path: "/genres/:id/tracks",
+        component: TrackList,
+        meta: {
+            title: "MainstreamSource | Genre {Name} |Track List",
+            metaTags: [
+                {
+                    name: "description",
+                    content: "MainstreamSource Genra Track List page",
+                },
+                {
+                    property: "og:description",
+                    content: "MainstreamSource Genra Track List page",
+                },
+            ],
         },
-        {
-          property: "og:description",
-          content: "MainstreamSource Genra Track List page",
-        },
-      ],
     },
-  },
+    {
+        path: '*',
+        name: 'catchAll',
+        component: HomePage,
+        meta: {
+            title: "MainstreamSource | Home",
+            metaTags: [
+                {
+                    name: "description",
+                    content: "MainstreamSource Home page",
+                },
+                {
+                    property: "og:description",
+                    content: "MainstreamSource Home page",
+                },
+            ],
+        },
+    }
 ];
 
 const router = new VueRouter({
-  routes,
-  mode: "history",
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    } else {
-      return { x: 0, y: 0 };
-    }
-  },
+    routes,
+    mode: "history",
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return {x: 0, y: 0};
+        }
+    },
 });
 
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title;
-  next();
+    document.title = to.meta.title;
+    next();
 });
 
 new Vue({
-  router,
-  render: (h) => h(App),
+    router,
+    render: (h) => h(App),
 }).$mount("#app");
