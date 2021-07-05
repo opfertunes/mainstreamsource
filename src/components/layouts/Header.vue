2eb0e5ee364edc40c44de1d4014c7578e162f946
John Opferkuch <opfertunes@gmail.com>
2021-07-05 11:23:40 -0700

    open side menu on hover

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,64 +1,74 @@
 <template>
-
-
-  <b-navbar toggleable="lg" type="dark" fixed="top" variant="dark" :sticky="true">
-   
-<img id="header-logo" src="@/assets/images/mss-logo.png" alt="mainstream source logo" @click="goHome"/>
-    <left-side-bar 
-        :props-is-collapsed="collapse" 
-        @toggle-sidebar="toggleSidebar">
+  <b-navbar
+    toggleable="lg"
+    type="dark"
+    fixed="top"
+    variant="dark"
+    :sticky="true"
+  >
+    <img
+      id="header-logo"
+      src="@/assets/images/mss-logo.png"
+      alt="mainstream source logo"
+      @click="goHome"
+    />
+    <left-side-bar
+      :props-is-collapsed="collapse"
+      @toggle-sidebar="toggleSidebar"
+    >
     </left-side-bar>
 
     <b-navbar-brand href="#">
-      <div class="d-inline-block pr-2" @click="switchToggle">
-         <div id="left-menu-toggler" class="icon-menu"/>
-      </div> 
-                   
-            
-              
-            
+      <div
+        class="d-inline-block pr-2"
+        @click="switchToggle"
+        @mouseover="startToggleHover"
+        @mouseout="endToggleHover"
+      >
+        <div id="left-menu-toggler" class="icon-menu" />
+      </div>
     </b-navbar-brand>
-   
-    
+
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav right>
-
       <b-navbar-nav class="ml-auto">
-        <b-nav-item href="https://mainstreamsource.com/">Main</b-nav-item> 
-        <b-nav-item href="#">License Trademark</b-nav-item> 
-        <b-nav-item href="https://mainstreamsource.com/news/">News</b-nav-item> 
-        <b-nav-item href="#">Contact</b-nav-item> 
-        <b-nav-item v-if="!isAuthenticated" 
-            href="https://mainstreamsource.com/login.php?redirect_to=/licensing/">Login</b-nav-item>
-        
-        <b-nav-item v-if="isAuthenticated" 
-            to="/projects">My Projects</b-nav-item>     
+        <b-nav-item href="https://mainstreamsource.com/">Main</b-nav-item>
+        <b-nav-item href="#">License Trademark</b-nav-item>
+        <b-nav-item href="https://mainstreamsource.com/news/">News</b-nav-item>
+        <b-nav-item href="#">Contact</b-nav-item>
+        <b-nav-item
+          v-if="!isAuthenticated"
+          href="https://mainstreamsource.com/login.php?redirect_to=/licensing/"
+          >Login</b-nav-item
+        >
 
-        <b-nav-item v-if="isAuthenticated" 
-            to="https://mainstreamsource.com/login.php?Action=logout&amp;redirect_to=/licensing/">Logout</b-nav-item>     
+        <b-nav-item v-if="isAuthenticated" to="/projects"
+          >My Projects</b-nav-item
+        >
 
-       
+        <b-nav-item
+          v-if="isAuthenticated"
+          to="https://mainstreamsource.com/login.php?Action=logout&amp;redirect_to=/licensing/"
+          >Logout</b-nav-item
+        >
       </b-navbar-nav>
     </b-collapse>
-    
   </b-navbar>
-
-  
 </template>
 
 <script>
-
 import LeftSideBar from "@/components/layouts/LeftSideBar";
-import {mapActions, mapGetters} from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "Header",
-  components: {LeftSideBar},
+  components: { LeftSideBar },
   props: {},
   data() {
     return {
-      collapse: true
+      collapse: true,
+      hovering: false,
     };
   },
   created() {
@@ -74,11 +84,22 @@ export default {
     switchToggle() {
       this.collapse = !this.collapse;
     },
-    ...mapActions("user", ["loadAuthUserData" ]),
+    startToggleHover() {
+      this.hovering = true;
+      setTimeout(() => {
+        if (this.hovering && this.collapse) {
+          this.switchToggle();
+        }
+      }, 500);
+    },
+    endToggleHover() {
+      this.hovering = false;
+    },
+    ...mapActions("user", ["loadAuthUserData"]),
   },
   computed: {
-     ...mapGetters("user", ["isAuthenticated" ]),
-  }
+    ...mapGetters("user", ["isAuthenticated"]),
+  },
 };
 </script>
 
